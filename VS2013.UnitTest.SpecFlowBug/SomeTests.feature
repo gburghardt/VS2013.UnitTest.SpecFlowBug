bbd61b0226e4682f1c7b2fa8dc6198a722dac4b7
Greg Burghardt <greg_burghardt@yahoo.com>
2015-06-15 14:27:33 -0400

    Dead simple MS Test unit test project created in VS2013 illustrating issue: https://github.com/techtalk/SpecFlow/issues/452

---
+++ VS2013.UnitTest.SpecFlowBug/SomeTests.feature
@@ -0,0 +1,17 @@
+﻿Feature: SomeTests
+	In order to avoid silly mistakes
+	As a math idiot
+	I want to be told the sum of two numbers
+
+Background:
+	Given everything is right with the world
+
+Scenario: True is true
+	Then true is true
+	And false is not true
+	And false is false
+
+Scenario: False is not true
+	Then true is true
+	And false is not true
+	And false is false
---
+++ VS2013.UnitTest.SpecFlowBug/Steps.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using TechTalk.SpecFlow;
+
+namespace VS2013.UnitTest.SpecFlowBug
+{
+    [Binding]
+    public sealed class Steps
+    {
+        [Given(@"everything is right with the world")]
+        public void GivenEverythingIsRightWithTheWorld()
+        {
+        }
+
+        [Then(@"true is true")]
+        public void ThenTrueIsTrue()
+        {
+            Assert.IsTrue(true);
+        }
+
+        [Then(@"false is not true")]
+        public void ThenFalseIsNotTrue()
+        {
+            Assert.IsFalse(false);
+        }
+
+        [Then(@"false is false")]
+        public void ThenFalseIsFalse()
+        {
+            Assert.IsTrue(false == false);
+        }
+    }
+}
